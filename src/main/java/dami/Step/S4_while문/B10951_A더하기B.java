c4bcd07e77f843e038010a3805d187f02697a106
DAMICHOI <choidami93@gmail.com>
2021-07-29 21:57:47 +0900

    style : Step 4 디렉토리, 제목 변경
    Step 4 디렉토리, 제목 변경

--- src/main/java/dami/Step/S4_while문/B10951_A더하기B.java
+++ src/main/java/dami/Step/S4_while문/B10951_A더하기B.java
@@ -1,21 +1,20 @@
-package dami.Step.Step4;
+package dami.Step.S4_while문;
 
 import java.io.*;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
-/*A+B - 4
-* [문제]
-* 두 정수와 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
-*
-* [입력]
-* 입력은 여러 개의 테스트 케이스로 이루어져 있다.
-* 각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0<A, B<10)
-*
-* [출력]
-* 각 테스트 케이스마다 A+B를 출력한다.
-* */
-public class S2_B10951_APlusB {
+public class B10951_A더하기B {
+    /*A+B - 4
+     * [문제]
+     * 두 정수와 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+     *
+     * [입력]
+     * 입력은 여러 개의 테스트 케이스로 이루어져 있다.
+     * 각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0<A, B<10)
+     *
+     * [출력]
+     * 각 테스트 케이스마다 A+B를 출력한다.
+     * */
     public static void main (String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
--- src/main/java/dami/Step/S4_while문/B10952_A더하기B.java
+++ src/main/java/dami/Step/S4_while문/B10952_A더하기B.java
@@ -1,9 +1,22 @@
-package dami.Step.Step4;
+package dami.Step.S4_while문;
 
 import java.io.*;
 import java.util.StringTokenizer;
 
-public class S1_B10952_APlusB {
+public class B10952_A더하기B {
+    /*
+    * A+B
+    * [문제]
+    * 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+    *
+    * [입력]
+    * 입력은 여러 개의 테스트 케이스로 이루어져 있다.
+    * 각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+    * 입력의 마지막에는 0 두 개가 들어온다.
+    *
+    * [출력]
+    * 각 테스트 케이스마다 A+B를 출력한다.
+    * */
     public static void main (String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;

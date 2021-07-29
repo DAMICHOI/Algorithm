3c1c5cac98fd89f2b8dccee0777cb6fa29d9c92e
DAMICHOI <choidami93@gmail.com>
2021-07-29 21:23:19 +0900

    style : Step 1 디렉토리, 제목 변경
    Step 1 디렉토리, 제목 변경

---
+++ src/main/java/dami/Step/S1_입출력과_사칙연산/B1000_A더하기B.java
@@ -0,0 +1,25 @@
+package dami.Step.S1_입출력과_사칙연산;
+
+import java.util.Scanner;
+
+public class B1000_A더하기B {
+    /*
+    * 1000. A+B
+    * [문제]
+    * 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+    *
+    * [입력]
+    * 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+    *
+    * [출력]
+    * 첫째 줄에 A+B를 출력한다.
+    * */
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+
+        int first = scanner.nextInt();
+        int second = scanner.nextInt();
+
+        System.out.println(first + second);
+    }
+}
---
+++ src/main/java/dami/Step/S1_입출력과_사칙연산/B10172_개.java
@@ -0,0 +1,24 @@
+package dami.Step.S1_입출력과_사칙연산;
+
+public class B10172_개 {
+    /*
+    * 10172. 개
+    * [문제]
+    * 아래 예제와 같이 개를 출력하시오.
+    *
+    * [입력]
+    * 없음.
+    *
+    * [출력]
+    * 개를 출력한다.
+    * */
+    public static void main(String args[]) {
+        String dog = "|\\_/|\n" +
+                "|q p|   /}\n" +
+                "( 0 )\"\"\"\\\n" +
+                "|\"^\"`    |\n" +
+                "||_/=\\\\__|";
+
+        System.out.println(dog);
+    }
+}

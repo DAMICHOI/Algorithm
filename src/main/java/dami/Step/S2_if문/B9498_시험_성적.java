fecb5780adb52afac0c28ddf698cf453b92cf259
DAMICHOI <choidami93@gmail.com>
2021-07-29 21:37:27 +0900

    style : Step 2 디렉토리, 제목 변경
    Step 2 디렉토리, 제목 변경

--- src/main/java/dami/Step/S2_if문/B9498_시험_성적.java
+++ src/main/java/dami/Step/S2_if문/B9498_시험_성적.java
@@ -1,8 +1,19 @@
-package dami.Step.Step2;
+package dami.Step.S2_if문;
 
 import java.util.Scanner;
 
-public class S2_B9498_시험_성적 {
+public class B9498_시험_성적 {
+    /*
+    * 시험 성적
+    * [문제]
+    * 시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램을 작성하시오.
+    *
+    * [입력]
+    * 첫째 줄에 시험 점수가 주어진다. 시험 점수는 0보다 크거나 같고, 100보다 작거나 같은 정수이다.
+    *
+    * [출력]
+    * 시험 성적을 출력한다.
+    * */
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
         int score = scanner.nextInt();
--- src/main/java/dami/Step/Step1/S1_B2557_HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package dami.Step.Step1;
-
-public class S1_B2557_HelloWorld {
-    public static void main(String args[]) {
-        System.out.println("Hello World!");
-    }
-}

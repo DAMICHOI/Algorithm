0d8331a40e6dab96503971be0a8509a5e2fc4bd3
Dami Choi <choidami93@gmail.com>
2021-12-13 12:54:37 +0900

    docs : Leetcode easy 문제 풀이
    - (1446) Consecutive Characters
    - (1) Two Sum

---
+++ src/main/java/dami/leetcode/L1446_Consecutive_Characters.java
@@ -0,0 +1,36 @@
+package dami.leetcode;
+
+public class L1446_Consecutive_Characters {
+	public static void main(String[] args) {
+		L1446_Consecutive_Characters Solution = new L1446_Consecutive_Characters();
+		System.out.println(Solution.maxPower("leetcode"));
+		System.out.println(Solution.maxPower("abbcccddddeeeeedcba"));
+		System.out.println(Solution.maxPower("triplepillooooow"));
+		System.out.println(Solution.maxPower("hooraaaaaaaaaaay"));
+		System.out.println(Solution.maxPower("tourist"));
+		System.out.println(Solution.maxPower("j"));
+	}
+
+	private int maxPower(String s) {
+		if (s == null || s.length() == 0) {
+			return 0;
+		}
+
+		int count = 0, maxCount = 0;
+		char prevC = ' ';
+
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			if (prevC == c) {
+				count++;
+			} else {
+				count = 1;
+				prevC = c;
+			}
+
+			maxCount = Math.max(count, maxCount);
+		}
+
+		return maxCount;
+	}
+}
---
+++ src/main/java/dami/leetcode/L1_Two_Sum.java
@@ -0,0 +1,43 @@
+package dami.leetcode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class L1_Two_Sum {
+	public static void main(String[] args) {
+		L1_Two_Sum solution = new L1_Two_Sum();
+		System.out.println(solution.twoSum(new int[] {2, 7, 11, 15}, 9));
+		System.out.println(solution.twoSum(new int[] {3, 2, 4}, 6));
+		System.out.println(solution.twoSum(new int[] {3, 3}, 6));
+	}
+
+	// 시간복잡도 O(N^2)
+	/*public int[] twoSum(int[] nums, int target) {
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = i + 1; j < nums.length; j++) {
+				if (nums[i] == target - nums[j]) {
+					System.out.println(i + " " + j);
+					return new int[] {i, j};
+				}
+			}
+		}
+		return null;
+	}*/
+
+	// 시간복잡도 O(N)
+	public int[] twoSum(int[] nums, int target) {
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int i = 0; i < nums.length; i++) {
+			// 현재 수(nums[i])의 보수가 있는지 확인
+			if (map.containsKey(nums[i])) {
+				// 있을 경우, 현재 수를 보수로 갖던 수의 index와 현재 index를 반환
+				return new int[] {map.get(nums[i]), i};
+			}
+			// 없을 경우, 현재 수의 보수 값을 key로 하고 현재 수의 index를 value로 하는 데이터 삽입
+			map.put(target - nums[i], i);
+		}
+		return new int[] {};
+	}
+}

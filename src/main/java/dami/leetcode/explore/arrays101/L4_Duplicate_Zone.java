cdf321a8f955b1f4d4efcb43fecef0903eab137c
Dami Choi <choidami93@gmail.com>
2021-12-28 10:19:19 +0900

    docs : LeetCode 문제 풀이 추가
    - Arrays 101 - Inserting Items Into an Array 문제 풀이 추가
    - (L876) Middle of the Linked List

---
+++ src/main/java/dami/leetcode/explore/arrays101/L4_Duplicate_Zone.java
@@ -0,0 +1,38 @@
+package dami.leetcode.explore.arrays101;
+
+class L4_Duplicate_Zone {
+	public static void main(String[] args) {
+		L4_Duplicate_Zone solution = new L4_Duplicate_Zone();
+		solution.duplicateZeros(new int[] {1, 0, 2, 3, 0, 4, 5, 0});
+		// solution.duplicateZeros(new int[] {0, 0, 0, 0, 0, 0, 0});
+	}
+
+	private void duplicateZeros(int[] arr) {
+		int zeros = 0;
+		int n = arr.length;
+
+		for (int i = 0; i < n; i++) {
+			if (arr[i] == 0)
+				zeros++;
+		}
+
+		for (int i = n - 1; i >= 0; i--) {
+			int curIndex = i + zeros;
+			if (curIndex < n) {
+				arr[curIndex] = arr[i];
+			}
+
+			if (arr[i] == 0) {
+				if (curIndex - 1 < n) {
+					arr[curIndex - 1] = arr[i];
+				}
+
+				zeros--;
+			}
+		}
+
+		for (int num : arr) {
+			System.out.println(num);
+		}
+	}
+}
---
+++ src/main/java/dami/leetcode/problems/L876_Middle_of_the_Linked_List.java
@@ -0,0 +1,86 @@
+package dami.leetcode.problems;
+
+import java.util.ArrayDeque;
+
+public class L876_Middle_of_the_Linked_List {
+	public static void main(String[] args) {
+		L876_Middle_of_the_Linked_List solution = new L876_Middle_of_the_Linked_List();
+
+		ListNode listNode = new ListNode(1);
+		listNode.next = new ListNode(2);
+		listNode.next.next = new ListNode(3);
+		listNode.next.next.next = new ListNode(4);
+		listNode.next.next.next.next = new ListNode(5);
+		System.out.println(solution.middleNode(listNode).toString());
+
+		listNode = new ListNode(1);
+		listNode.next = new ListNode(2);
+		listNode.next.next = new ListNode(3);
+		listNode.next.next.next = new ListNode(4);
+		listNode.next.next.next.next = new ListNode(5);
+		listNode.next.next.next.next.next = new ListNode(6);
+		System.out.println(solution.middleNode(listNode).toString());
+
+		listNode = new ListNode(1);
+		System.out.println(solution.middleNode(listNode).toString());
+	}
+
+	private ListNode middleNode(ListNode head) {
+		if (head == null && head.next == null) {
+			return null;
+		}
+
+		ArrayDeque<ListNode> deque = new ArrayDeque<>();
+		ListNode headCopy = head;
+
+		while (headCopy != null) {
+			deque.push(headCopy);
+			headCopy = headCopy.next;
+		}
+
+		int count = deque.size() % 2 == 0 ? deque.size() / 2 : deque.size() / 2 + 1;
+		ListNode top = null;
+
+		while (count-- > 0) {
+			top = deque.pop();
+		}
+
+		return top;
+	}
+
+	/*private ListNode middleNode(ListNode head) {
+		ListNode slow = head;
+		ListNode fast = head;
+		while (fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		return slow;
+	}*/
+
+}
+
+class ListNode {
+	int val;
+	ListNode next;
+
+	public ListNode() {
+	}
+
+	public ListNode(int val) {
+		this.val = val;
+	}
+
+	public ListNode(int val, ListNode next) {
+		this.val = val;
+		this.next = next;
+	}
+
+	@Override
+	public String toString() {
+		return "ListNode{" +
+			"val=" + val +
+			", next=" + next +
+			'}';
+	}
+}
